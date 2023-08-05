8fa10928ddd0e44817249e24528b62b7a6ec3f47
Someone <120342697+Somespi@users.noreply.github.com>
2023-08-06 02:33:27 +0400

    Messages: add name header

---
+++ src/lib/Messages.svelte
@@ -0,0 +1,7 @@
+<div class="h-full bg-[#313338] flex-1">
+    <div class="shadow-lg p-4 mb-11">
+        <h1 class="text-[#f0f1f3] font-semibold ">
+            💬・general
+        </h1>
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 
 <script lang="ts">
 	import ChannelsList from '$lib/ChannelsList.svelte';
+	import Messages from '$lib/Messages.svelte';
     import Servers from '$lib/Servers.svelte';
 </script>
 
 <div class="flex flex-row h-screen w-full">
     <Servers/>
     <ChannelsList/>
+    <Messages/>
 </div>
 
     
