1c174897b2f090ba23c7fe3b35bb0163f500f14d
Someone <120342697+Somespi@users.noreply.github.com>
2023-08-14 02:53:32 +0400

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port: 3000,
 		proxy: {
-			'api': 'http://localhost:8080/api'
+			'/api': {
+				target: 'http://localhost:8080/api',
+				changeOrigin: true
+			}
 		}
 	}
 });
