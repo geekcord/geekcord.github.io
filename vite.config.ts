52948e7ab40440278d65fb88d2512039778e793a
Someone <120342697+Somespi@users.noreply.github.com>
2023-08-14 02:42:23 +0400

    Update vite.config.ts
    Co-authored-by: Abdelrahman Dwedar <dedou2005@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 3000,
+		proxy: {
+			'api': 'http://localhost:8080/api'
+		}
+	}
 });
